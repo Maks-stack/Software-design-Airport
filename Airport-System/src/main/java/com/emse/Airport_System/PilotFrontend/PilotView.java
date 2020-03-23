a538b0056370d7e9766366fda76f8456b1dac716
maks-stack <maxmenschel5@gmail.com>
2020-03-23 14:35:52 +0100

    Added pilot Interface

---
+++ Airport-System/src/main/java/com/emse/Airport_System/PilotFrontend/PilotView.java
@@ -0,0 +1,14 @@
+package com.emse.Airport_System.PilotFrontend;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+
+public class PilotView {
+	 @RequestMapping("/PilotInterface")
+	    String hello() {
+	        return "Create new Pilot";
+	    } 
+}
