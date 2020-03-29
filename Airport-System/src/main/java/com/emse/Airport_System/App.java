f2577364d03a3153f92933719158d918f1e92a86
DomantasVar <domantasvarapnickas@gmail.com>
2020-03-29 15:16:51 +0200

    pre stash pop

--- Airport-System/src/main/java/com/emse/Airport_System/App.java
+++ Airport-System/src/main/java/com/emse/Airport_System/App.java
@@ -1,5 +1,6 @@
 package com.emse.Airport_System;
 
+import com.emse.Airport_System.Plane.PlaneManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,6 @@ public class App {
 	public static void main(String Args[]) {
     	// line added by Luis to test commit from Eclipse.....
         SpringApplication.run(App.class, Args);
-        //PlaneManager.mainloop();
+        PlaneManager.mainloop();
 	}
 }
