53d65a23d9405a763ffe632aefacf0cf8c8783f1
DomantasVar <domantasvarapnickas@gmail.com>
2020-04-11 18:39:38 +0200

    Exception handling - improved ControllerAdvice

--- Airport-System/src/main/java/com/emse/airportSystem/GlobalControllerExceptionHandler.java
+++ Airport-System/src/main/java/com/emse/airportSystem/GlobalControllerExceptionHandler.java
@@ -2,17 +2,19 @@ package com.emse.airportSystem;
 
 import com.emse.airportSystem.exceptions.ServiceNotAvailableException;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-
 @ControllerAdvice
 class GlobalControllerExceptionHandler {
+
     @ExceptionHandler(ServiceNotAvailableException.class)
-    public ResponseEntity<?> handleServiceNotAvailableException(Exception ex) {
-        return new ResponseEntity<>(HttpStatus.CONFLICT);
+    public ResponseEntity<BackendErrorResponse> handleServiceNotAvailableException(Exception ex) {
+        HttpHeaders responseHeaders = new HttpHeaders();
+        return new ResponseEntity<>(new BackendErrorResponse(ex), responseHeaders, HttpStatus.CONFLICT);
     }
 
 }
