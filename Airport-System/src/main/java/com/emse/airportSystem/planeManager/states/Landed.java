6e27ce938794c8b131373aa76e0be3ef34928022
SophieLIF <48492909+SophieLIF@users.noreply.github.com>
2020-04-29 18:42:38 +0200

    Add new state: AwaitingGateAssignment

--- Airport-System/src/main/java/com/emse/airportSystem/planeManager/states/Landed.java
+++ Airport-System/src/main/java/com/emse/airportSystem/planeManager/states/Landed.java
@@ -3,7 +3,7 @@ package com.emse.airportSystem.planeManager.states;
 public class Landed implements State {
 
 	public State proceedToNextState() {
-		return new AtTerminal();
+		return new AwaitingGateAssignment();
 	}
 
 }
