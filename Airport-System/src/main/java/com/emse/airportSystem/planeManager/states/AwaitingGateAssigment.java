8fac1fde54f342eec36019b877962ed648c7fa52
Lyudmila Vatskicheva <lyudmila.vatskicheva@gmail.com>
2020-04-30 16:13:51 +0200

    change next state in AwaitingGateAssignment to be AtTerminal, instead of TakingOff

--- Airport-System/src/main/java/com/emse/airportSystem/planeManager/states/AwaitingGateAssigment.java
+++ Airport-System/src/main/java/com/emse/airportSystem/planeManager/states/AwaitingGateAssigment.java
@@ -2,7 +2,7 @@ package com.emse.airportSystem.planeManager.states;
 
 public class AwaitingGateAssigment implements State {
 	public State proceedToNextState() {
-		return new TakingOff();
+		return new AtTerminal();
 	}
 
 	public String getStateName(){
